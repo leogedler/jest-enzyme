4f46deb1004762691045ffd2f472e52f2969194b
Leonardo Gedler <leonardo.gedler@macys.com>
2018-11-12 14:56:07 -0300

    Added integration test

---
+++ src/__tests__/integrations.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import moxios from 'moxios';
+import Root from 'Root';
+import App from 'components/App';
+
+beforeEach(() => {
+  moxios.install();
+  moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+    status: 200,
+    response: [{ name: 'Feched #1' }, { name: 'Fetched #2' }]
+  });
+});
+
+afterEach(() => {
+  moxios.uninstall();
+})
+
+it('can fetch a list of comments and display them', (done) => {
+  // Attempt to render the entire app
+  const wrapped = mount(
+    <Root>
+      <App />
+    </Root>
+  );
+
+  // Find the fetchComments button and click it
+  wrapped.find('.fetch-comments').simulate('click');
+
+  // introduce a Tiny little pause using moxios wait
+  moxios.wait(() => {
+
+    // Update component
+    wrapped.update();
+
+    // Expect to find a list of comments
+    expect(wrapped.find('li').length).toEqual(2);
+  
+    done();
+    wrapped.unmount();
+  });
+});
